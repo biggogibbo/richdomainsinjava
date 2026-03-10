f1cc1dcfe0d5b27573d47072e8e078e143a6d313
dgibson <dgibson@roku.com>
2026-03-10 11:10:08 +0000

    used lombok

--- src/main/java/com/roku/richdomains/domain/Customer.java
+++ src/main/java/com/roku/richdomains/domain/Customer.java
@@ -1,21 +1,13 @@
 package com.roku.richdomains.domain;
 
 import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
+@AllArgsConstructor
 public class Customer {
   private final String id;
   private final List<String> accountIds;
 
-  public Customer(String id, List<String> accountIds) {
-    this.id = id;
-    this.accountIds = accountIds;
-  }
-
-  public String getId() {
-    return id;
-  }
-
-  public List<String> getAccountIds() {
-    return accountIds;
-  }
 }
